d4496630d0f9162901dba39fe3493affd9882650
devdussey <228256194+devdussey@users.noreply.github.com>
2025-11-06 22:57:24 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect, useState } from 'react';
 import type { Session } from '@supabase/supabase-js';
 import { Navigate, Route, Router, Routes, supabase } from './lib';
